9fecc9b81de703268b34db82ca21c760bd650464
Kadher111 <kadher2001@gmail.com>
2025-01-09 00:27:09 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Task1 from './Pages/Task1';
-import Task2 from './Pages/Task2';
-import Task3 from './Pages/Task3';
-import Home from './Pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Task1 from "./Pages/Task1";
+import Task2 from "./Pages/Task2";
+import Task3 from "./Pages/Task3";
+import Home from "./Pages/Home";
+import DataFetching from "./components/DataFetching";
 
-
-export default function App(){
+export default function App() {
   return (
     <div>
       <BrowserRouter>
@@ -13,9 +13,10 @@ export default function App(){
           <Route path="/Home" element={<Home />} />
           <Route path="/Task1" element={<Task1 />} />
           <Route path="/Task2" element={<Task2 />} />
-          <Route path="/Task3" element={<Task3 />}/>
+          <Route path="/Task3" element={<Task3 />} />
+          <Route path="/df" element={<DataFetching />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
--- src/Pages/Task3.jsx
+++ src/Pages/Task3.jsx
@@ -1,9 +1,24 @@
-import ComingSoon from "../components/ComingSoon";
+// import ComingSoon from "../components/ComingSoon";
 import * as React from "react";
+import { useEffect, useState } from "react";
 export default function Task2() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const logMousePosition = (e) => {
+    console.log("Mouse Event");
+    setX(e.clientX);
+    setY(e.clientY);
+  };
+
+  useEffect(() => {
+    console.log("UseEffect Called");
+    window.addEventListener("mousemove", logMousePosition);
+
+  }, []);
   return (
-    <>
-      <ComingSoon />
-    </>
+    <div>
+      Hooks X - {x} Y - {y}
+    </div>
   );
 }
